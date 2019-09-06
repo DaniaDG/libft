486670ccf3bb7e51bcd607ff1429c6799d19eaee
Bari Sausages <bsausage@ox-t1.21-school.ru>
2019-09-06 20:14:40 +0300

    itoa

---
+++ ft_itoa.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsausage <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/06 18:51:12 by bsausage          #+#    #+#             */
+/*   Updated: 2019/09/06 18:51:16 by bsausage         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static int	num_of_letters(int n)
+{
+	int i;
+
+	i = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		i++;
+	while (n != 0)
+	{
+		n = n / 10;
+		i++;
+	}
+	return (i);
+}
+
+char		*ft_itoa(int n)
+{
+	char	*str;
+	int		len;
+
+	len = num_of_letters(n);
+	str = NULL;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (n == 0)
+		str[0] = '0';
+	if (str)
+	{
+		str[len--] = '\0';
+		if (n < 0)
+		{
+			str[0] = '-';
+			str[len--] = (n % 10) * (-1) + '0';
+			n = (n / 10) * (-1);
+		}
+		while (n)
+		{
+			str[len--] = n % 10 + '0';
+			n = n / 10;
+		}
+	}
+	return (str);
+}
--- libft.h
+++ libft.h
@@ -17,6 +17,7 @@
 
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
+char	*ft_itoa(int n);
 size_t	ft_strlen(const char *str);
 int		ft_isalpha(int ch);
 int		ft_isdigit(int ch);
